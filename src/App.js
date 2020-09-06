e45950f58a3dbb51793e4811bfc436bd23db77fe
Abdullah Sadaqa <eng.abdullahhasan2016@gmail.com>
2020-09-06 22:09:31 +0200

    Pass Contacts Into Components (#3)
    * Pass Contacts Into Components
    
    * refactor to stateless functional component
    
    * Add State Component
    
    * Update state with setState

--- src/App.js
+++ src/App.js
@@ -1,9 +1,45 @@
 import React, { Component } from "react";
+import ListContacts from "./ListContacts";
 
 class App extends Component {
+  state = {
+    contacts: [
+      {
+        id: "karen",
+        name: "Karen Isgrigg",
+        handle: "@karen_isgrigg",
+        avatarURL: "http://localhost:5001/karen.jpg",
+      },
+      {
+        id: "richard",
+        name: "Richard Kalehoff",
+        handle: "@richardkalehoff",
+        avatarURL: "http://localhost:5001/richard.jpg",
+      },
+      {
+        id: "tyler",
+        name: "Tyler McGinnis",
+        handle: "@tylermcginnis",
+        avatarURL: "http://localhost:5001/tyler.jpg",
+      },
+    ],
+  };
+  removeContact = (contact) => {
+    this.setState((currentContacts) => ({
+      contacts: currentContacts.contacts.filter((filterdContact) => {
+        return filterdContact.id !== contact.id;
+      }),
+    }));
+  };
   render() {
-    return <div>Hello World</div>;
+    return (
+      <div>
+        <ListContacts
+          contacts={this.state.contacts}
+          onDeleteContact={this.removeContact}
+        />
+      </div>
+    );
   }
 }
-
 export default App;
